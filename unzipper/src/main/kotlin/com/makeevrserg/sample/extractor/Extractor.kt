b88c752a474fc8f0d0add3c77ac83f6f4fe80e99
Roman Makeev <makeevrserg@gmail.com>
2023-10-09 21:09:02 +0300

    added extractors

---
+++ unzipper/src/main/kotlin/com/makeevrserg/sample/extractor/Extractor.kt
@@ -0,0 +1,29 @@
+package com.makeevrserg.sample.extractor
+
+import com.makeevrserg.sample.jvm.JvmExtractor
+import java.io.File
+import okio.FileSystem
+import okio.Path.Companion.toOkioPath
+import unzip
+
+sealed class Extractor(val name: String) {
+    abstract fun extract(zipFile: File, zipOutputFolder: File)
+
+    data object Java : Extractor("Java") {
+        override fun extract(zipFile: File, zipOutputFolder: File) {
+            val newOutputFolder = File(File(zipOutputFolder, "jvm"), zipFile.name)
+            if (newOutputFolder.exists()) newOutputFolder.delete()
+            if (!newOutputFolder.exists()) newOutputFolder.mkdirs()
+            JvmExtractor.unzip(zipFile, newOutputFolder)
+        }
+    }
+
+    data object Okio : Extractor("Okio") {
+        override fun extract(zipFile: File, zipOutputFolder: File) {
+            val newOutputFolder = File(File(zipOutputFolder, "okio"), zipFile.name)
+            if (newOutputFolder.exists()) newOutputFolder.delete()
+            if (!newOutputFolder.exists()) newOutputFolder.mkdirs()
+            FileSystem.SYSTEM.unzip(zipFile.toOkioPath(), newOutputFolder.toOkioPath())
+        }
+    }
+}
