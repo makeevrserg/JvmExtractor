7df3ef35fc7d8e8aeca0cc152e0229012832e03c
Roman Makeev <makeevrserg@gmail.com>
2023-10-09 21:09:10 +0300

    added main

---
+++ unzipper/src/main/kotlin/com/makeevrserg/sample/main.kt
@@ -0,0 +1,34 @@
+package com.makeevrserg.sample
+
+import com.makeevrserg.sample.extractor.Extractor
+import java.io.File
+
+const val ZIP_FILE_PATH = "./root/test.zip"
+const val ZIP_OUTPUT_PATH = "./root"
+
+fun main() {
+    val zipFile = File(ZIP_FILE_PATH)
+    val zipOutputFolder = File(ZIP_OUTPUT_PATH)
+    if (!zipFile.exists()) {
+        println("File ${zipFile.absolutePath} not found!")
+        return
+    }
+    println("Started extraction of ${zipFile.absolutePath}")
+    println("Extracting into ${zipOutputFolder.absolutePath}")
+    val extractors = listOf(
+        Extractor.Java,
+        Extractor.Okio
+    )
+    extractors.forEach { extractor ->
+        val result = runCatching {
+            extractor.extract(zipFile, zipOutputFolder)
+        }
+        result.onFailure { throwable ->
+            println("Could not finish extraction of ${extractor.name}")
+            throwable.printStackTrace()
+        }
+        result.onSuccess {
+            println("Extractor '${extractor.name}' extracted files successfully")
+        }
+    }
+}
